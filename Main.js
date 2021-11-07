ff0c5c1c7d522abf2d6065f6a1b3e42ced5dfdf1
Magen Hocker <70960438+MagenHocker@users.noreply.github.com>
2021-11-06 23:58:50 -0400

    Implementing Queues and Stacks

--- Main.js
+++ Main.js
@@ -72,6 +72,10 @@ wizard2.introduce();
   - DYNAMIC ARRAYS --> Arrays that are subject to change on size
                    --> PC manages memory for you
 
+  - Cache Locality: The hardware in computers is designed to access memory
+                    close to one another so it make arrays slightly faster
+                    than datatype that store info at decentralized addresses
+
  */
 
 const strings= ['a', 'b', 'c', 'd'];
@@ -435,6 +439,16 @@ console.log(rotateBetter(arrayK, 3));
   - Maps maintain insertion order in javaScript
   - Sets only store the keys
 
+  Pro:
+  - Fast Lookups
+  - Fast Inserts
+  - Flexible Keys
+
+  Cons:
+  - Unordered
+  - Slow Key iteration
+  - Potential collisions
+
  */
 
 let user = {
@@ -558,6 +572,8 @@ console.log(hashTableA.keys());
 
  */
 
+// O(n) -- time complexity
+// O(n) -- space complexity
 const firstRepeatingValue = function (array) {
   if (!array.length) {
     return undefined;
@@ -594,3 +610,572 @@ console.log(firstRepeatingValue(arrayP));
 const arrayQ = [2, 1, 3, 1, 3, 2, 1];
 console.log(firstRepeatingValue(arrayQ));
 
+/*
+    ---------------------------- Linked Lists ------------------------------
+
+    [head node] ---> [node] ---> [node] ---> [tail node] ---> null
+    [----5----]      [-10-]      [-12-]      [----2----]
+
+    - Linked lists are null terminated
+    - a pointer points to the next element in the list
+
+    - JS does not have a linked list data structure but Java does
+    - There is singly and doubly linked list
+
+    - Doubly linked lists allow nodes to point to one another meaning you
+      can traverse backwards because they have an additional prev pointer
+
+    [head node] ---> [node] ---> [node] ---> [tail node] ---> null
+    [----5----] <--- [-10-] <--- [-12-] <--- [----2----]
+
+    - You SHOULD USE A SINGLY LINKED LIST when you need to use less space,
+      you DON'T need to traverse backwards or search, and you want
+      FAST insertion and deletion
+
+    - You SHOULD USE A DOUBLY LINKED LIST when you want to traverse backwards,
+      DON'T care about memory and you AREN'T frequently inserting and deleting,
+      but you do want to search
+
+    downside - a little more memory
+
+    - Why might linked lists be better than hashmap or arrays?
+      1.) They are ordered
+      2.) You can access and manipulate both the front and back of list
+      3.) When you remove/insert a value the pointers just adjusts to the new
+          or deleted node as opposed to reassigning all the values like a list
+      4.) Traversal is the same as array iteration except you don't know
+          how long the list will be but traversal is longer bc of computer
+          hardware
+
+     - Arrays are good bc computers are designed to find addresses next to one
+       another as opposed to scattered addresses
+
+       O(1) - Prepend
+       O(1) - Append
+       O(n) - lookup
+       O(n) - insert (have to go 1 by 1 to find where to place)
+       O(n) - delete
+
+       *insert and delete are still on average better in linked list than w/ arr
+
+     - What is a pointer?
+       A pointer is a reference to another object, node etc in memory
+
+
+     Pros:
+     Fast Insertion and Deletion
+     Ordered
+     Flexible Size
+
+     Cons:
+     Slow Lookup
+     More memory
+
+ */
+
+
+// Pointer example
+const obj1 = { a: true};
+const obj2 = obj1; // This a pointer by referencing the same object at the same address
+
+console.log('obj1', obj1);
+console.log('obj2', obj2);
+
+obj1.a = 'booya';
+
+console.log('obj1', obj1);
+console.log('obj2', obj2);
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new Node(value);
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array =[];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+    }
+
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    let leader = this.traverseToIndex(index-1);
+    const tempPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = tempPointer;
+    this.length++;
+  }
+
+  remove(index) {
+    if (index === 0) {
+      this.head = this.head.next;
+      return this.printList();
+    } else {
+      let leader = this.traverseToIndex(index-1);
+      let deletedNote = leader.next;
+      let follower = deletedNote.next;
+      leader.next = follower;
+      this.length--;
+      return this.printList();
+    }
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+  reverse() {
+
+    if (!this.head.next) {
+      return this.head;
+    }
+
+    let first = this.head;
+    this.tail = this.head;
+    let second = this.head.next;
+
+    while (second) {
+      const temp = second.next;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this.printList();
+  }
+
+}
+
+const linkedListA = new LinkedList(10);
+linkedListA.append(5);
+linkedListA.append(15);
+linkedListA.prepend(20);
+
+console.log(linkedListA);
+console.log(linkedListA.printList())
+linkedListA.insert(2, 7);
+console.log(linkedListA.printList())
+console.log(linkedListA);
+
+linkedListA.remove(2);
+console.log(linkedListA.printList());
+linkedListA.remove(0);
+console.log(linkedListA.printList());
+
+linkedListA.reverse();
+console.log('reversed', linkedListA.printList());
+
+console.log('-------- double linked list --------');
+
+class DNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null,
+      prev: null
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+  append(value) {
+    const newNode = new DNode(value);
+    this.tail.next = newNode;
+    newNode.prev = this.tail;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = new DNode(value);
+
+    newNode.next = this.head;
+    this.head.prev = newNode;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  printList() {
+    const array =[];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+    }
+
+    if (index >= this.length) {
+      return this.append(value);
+    }
+
+    const newNode = new DNode(value);
+    let leader = this.traverseToIndex(index-1);
+    const tempPointer = leader.next;
+    tempPointer.prev = newNode;
+    leader.next = newNode;
+    newNode.next = tempPointer;
+    newNode.prev = leader;
+    this.length++;
+  }
+
+  remove(index) {
+    if (index === 0) {
+      this.head = this.head.next;
+      return this.printList();
+    } else {
+      let leader = this.traverseToIndex(index-1);
+      let deletedNote = leader.next;
+      let follower = deletedNote.next;
+      deletedNote.prev = leader;
+      leader.next = follower;
+      this.length--;
+      return this.printList();
+    }
+  }
+
+  traverseToIndex(index) {
+    let counter = 0;
+    let currentNode = this.head;
+    while(counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
+
+}
+
+
+const doublyLinkedListA = new DoublyLinkedList(10);
+console.log(doublyLinkedListA.printList());
+
+doublyLinkedListA.append(5);
+console.log(doublyLinkedListA.printList());
+
+doublyLinkedListA.prepend(15);
+console.log(doublyLinkedListA.printList());
+
+doublyLinkedListA.insert(1, 7);
+console.log(doublyLinkedListA.printList());
+
+doublyLinkedListA.remove(2);
+console.log(doublyLinkedListA.printList());
+
+/*
+  ---------------------- Stacks and Queues --------------------------
+
+  Stacks: A type of data structure that are similar to stacked plates -- you
+          can only access the first plate and iterate through to grab a specific
+          plate - LIFO 'Last in First Out'
+
+  O(n) -- lookup
+  O(1) -- pop
+  O(1) -- push
+  O(1) -- peek
+
+  Browser History
+  3. [youtube]
+  2. [udemy]
+  1. [google]
+
+  Queues: Similar to a roller coaster line with the FIFO principal 'first in
+          first out'
+
+  Ideally you would want to implement a queue with a linked list
+
+  O(n) -- lookup
+  O(1) -- enqueue
+  O(1) -- dequeue
+  O(1) -- peek
+
+  Wait List
+  1. [Matt]
+  2. [Joy]
+  3. [Samir]
+  4. [Pavel]
+
+  Pros:
+  1.) Fast Operations
+  2.) Fast Peek
+  3.) Ordered
+
+  Cons:
+  1.) Slow lookup
+
+ */
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.bottom = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.top = newNode;
+      this.bottom = newNode;
+    } else {
+      let tempNode = this.top;
+      this.top = newNode;
+      newNode.next = tempNode;
+
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.top) {
+      return null;
+    }
+
+    if (this.isEmpty()) {
+      this.bottom = null;
+    }
+
+    const tempNode = this.top; // garbage collected language like JS automatically removed
+    this.top = this.top.next;
+    this.length--;
+    return tempNode;
+  }
+
+  isEmpty() {
+    return (this.length < 1);
+  }
+}
+
+const stackA = new Stack();
+stackA.push(1);
+stackA.push(2);
+stackA.push(3);
+
+
+console.log(stackA);
+console.log(stackA.peek());
+
+stackA.pop();
+console.log(stackA);
+
+
+class StackArray {
+  constructor() {
+    this.array = [];
+  }
+
+  peek() {
+    return this.array[this.array.length-1];
+  }
+
+  push(value) {
+    return this.array.push(value);
+  }
+
+  pop() {
+    return this.array.pop();
+  }
+
+  isEmpty() {
+    return this.array.isEmpty();
+  }
+}
+
+console.log('----------- stacks array -------------')
+
+const stackArrayA = new StackArray();
+stackArrayA.push(10);
+stackArrayA.push(15);
+stackArrayA.push(20);
+
+console.log(stackArrayA);
+
+stackArrayA.pop();
+console.log(stackArrayA);
+
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.first;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if (this.isEmpty()) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      const tempNode = this.last;
+      tempNode.next = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    if (this.first === this.last) {
+      this.last = null;
+    }
+
+    const deletedNode = this.first;
+    this.first = this.first.next;
+    this.length--;
+    return deletedNode;
+  }
+
+  isEmpty() {
+    return (this.length < 1);
+  }
+}
+
+const queueA = new Queue();
+queueA.enqueue("Tim");
+queueA.enqueue("Silas");
+queueA.enqueue("Margarie");
+console.log(queueA);
+
+queueA.dequeue();
+console.log(queueA);
+
+
+/*
+
+   Interview Problem: Implement a Queue Using Stacks
+
+
+
+ */
+
+class QueueS {
+  constructor() {
+    this.first = [];
+    this.last = [];
+  }
+
+  peek() {
+    if (this.last.length > 0) {
+      return this.last[0];
+    }
+
+    return this.first[this.first.length - 1];
+
+  }
+
+  enqueue(value) {
+    const length = this.first.length;
+    for (let i = 0; i < length; i++) {
+      this.last.push(this.first.pop());
+    }
+    this.last.push(value);
+    return this;
+  }
+
+  dequeue() {
+    const length = this.last.length;
+    for (let i = 0; i < length; i++) {
+      this.first.push(this.last.pop());
+    }
+    this.first.pop();
+    return this;
+  }
+
+  isEmpty() {
+    return (this.last.length < 1 && this.first.length < 1);
+  }
+}
+
+const queueB = new QueueS();
+queueB.enqueue("Tom");
+console.log(queueB);
+queueB.enqueue("Brady");
+console.log(queueB);
+
+queueB.enqueue("Ryan");
+queueB.enqueue("Reynolds");
+console.log(queueB);
+
+queueB.dequeue();
+console.log(queueB);
+
+queueB.dequeue()
+console.log(queueB);
